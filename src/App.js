11e781128ae901420088e5a48db9fe1d102649af
jessicaseto <jes.seto@gmail.com>
2018-11-20 17:34:43 -0800

    Removed unnecessary import dependencies from App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Link } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 // import * as BooksAPI from './BooksAPI'
 import MyReads from './MyReads'
 import SearchBooks from './SearchBooks'
