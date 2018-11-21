0f643ca2f7abf72391d1e996a84697c13ea302ed
jessicaseto <jes.seto@gmail.com>
2018-11-20 17:59:18 -0800

    Modified Book component to retrieve title, author, and backgroundImage from state

--- src/Book.js
+++ src/Book.js
@@ -3,19 +3,21 @@ import ShelfControl from './ShelfControl'
 
 class Book extends React.Component {
   state = {
-
-  }
+    title: 'To Kill a Mockingbird',
+    author: 'Harper Lee',
+    backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")'
+  };
 
   render() {
     return (
       <li>
         <div className="book">
           <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: this.state.backgroundImage}}></div>
             <ShelfControl/>
           </div>
-          <div className="book-title">To Kill a Mockingbird</div>
-          <div className="book-authors">Harper Lee</div>
+          <div className="book-title">{this.state.title}</div>
+          <div className="book-authors">{this.state.author}</div>
         </div>
       </li>
     );
