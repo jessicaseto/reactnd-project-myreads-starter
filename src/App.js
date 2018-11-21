f570d8f53d1907b287e1e18bb6ca51d0f2d8f457
jessicaseto <jes.seto@gmail.com>
2018-11-21 11:58:34 -0800

    Added books to state in App.js to pass to MyReads and SearchBooks componenets as a prop

--- src/App.js
+++ src/App.js
@@ -6,15 +6,19 @@ import SearchBooks from './SearchBooks'
 import './App.css'
 
 class BooksApp extends React.Component {
+  state = {
+    books: ''
+  };
+
   render() {
     return (
       <div className="app">
         <Route exact path="/" render={() => (
-            <MyReads/>
+            <MyReads books={this.state.books}/>
           )}
         />
         <Route path="/search" render={() => (
-            <SearchBooks/>
+            <SearchBooks books={this.state.books}/>
           )}
         />
       </div>
