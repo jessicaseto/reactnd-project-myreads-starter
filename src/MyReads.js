17fbff7ec2e5a1c1054f6868d3150fa3a2307de9
jessicaseto <jes.seto@gmail.com>
2018-11-21 17:46:49 -0800

    Improve formatting

--- src/MyReads.js
+++ src/MyReads.js
@@ -15,7 +15,10 @@ class MyReads extends React.Component {
               <h2 className="bookshelf-title">Currently Reading</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
-                  {this.props.books.filter((book) => (book.shelf === 'currentlyReading'))
+                  {this.props.books
+                    .filter((book) => (
+                      book.shelf === 'currentlyReading'
+                    ))
                     .map((book) => (
                       <li key={book.id}>
                         <Book book={book}/>
