f5d032945e34325e923335c4a1ff0cb2a13c0b5c
jessicaseto <jes.seto@gmail.com>
2018-11-21 17:42:08 -0800

    Added getAll to componentDidMount to get shelvedBooks

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import { Route } from 'react-router-dom'
 // import * as BooksAPI from './BooksAPI'
 import MyReads from './MyReads'
 import SearchBooks from './SearchBooks'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
   state = {
     shelvedBooks: []
   };
+
+  componentDidMount() {
+    BooksAPI.getAll().then(
+      (books) => {
+        this.setState({shelvedBooks: books})
+      }
+    );
+  }
 
   render() {
     return (
