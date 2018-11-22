fe46856808167c8a7d470f72a883251adf670817
jessicaseto <jes.seto@gmail.com>
2018-11-21 17:25:01 -0800

    Removed state from Book component

--- src/Book.js
+++ src/Book.js
@@ -2,13 +2,6 @@ import React from 'react'
 import ShelfControl from './ShelfControl'
 
 class Book extends React.Component {
-  state = {
-    title: 'To Kill a Mockingbird',
-    author: 'Harper Lee',
-    backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
-    shelf: 'none'
-  };
-
   /* Method to update shelf on change in select element */
   changeShelf = (shelf) => {
     this.setState((state) => ({ shelf }));
