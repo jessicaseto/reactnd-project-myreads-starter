faac79acec907e9c41b7a1e2de6161f5e4f495b7
jessicaseto <jes.seto@gmail.com>
2018-11-21 22:59:49 -0800

    Rewrote updateBookshelf function as arrow function to preserve context

--- src/App.js
+++ src/App.js
@@ -11,24 +11,24 @@ class BooksApp extends React.Component {
     shelvedBooks: []
   };
 
-  updateBookshelf(book, shelf) {
-    BooksAPI.update(book, shelf).then(() => (
-      BooksAPI.getAll().then(
-        (books) => {
-          this.setState({shelvedBooks: books})
-        }
-      )
-    )).catch((e) =>
-      console.log(e)
-    )
+  getShelvedBooks = () => {
+    BooksAPI.getAll().then(
+      (books) => {
+        this.setState({shelvedBooks: books});
+      }
+    );
+  }
+
+  updateBookshelf = (book, shelf) => {
+    BooksAPI.update(book, shelf)
+      .then(this.getShelvedBooks)
+      .catch((e) =>
+        console.log(e)
+      );
   }
 
   componentDidMount() {
-    BooksAPI.getAll().then(
-      (books) => {
-        this.setState({shelvedBooks: books})
-      }
-    );
+    this.getShelvedBooks();
   }
 
   render() {
